df862f7a73c1c46da2690e074debc67ac9d7a51d
freemind <freemind@freemind>
2013-09-26 00:55:07 +0200

    examples

--- ideas/snippets/server.py
+++ ideas/snippets/server.py
@@ -7,7 +7,7 @@ Created on 25.09.2013
 # Echo server program
 import socket
 
-HOST = '*'                 # Symbolic name meaning all available interfaces
+HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
