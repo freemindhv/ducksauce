ac42c592eeea9c15c4c2ab26dffea3492754b65f
freemindhv <freemind@highvillage.de>
2013-09-28 01:47:23 +0200

    1st version of server socket

--- server/src/main.py
+++ server/src/main.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python3
+import socket
+import select
 
+host = socket.gethostbyname(socket.gethostname())
+port = 51001
+buffsize = 4096
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    s.bind((host,port))
+except socket.error as msg:
+    print("Could not open socket, maybe the port is already used?")
+s.listen(5)
+
+while True:
+    inputready,outputready,exceptready = select.select([s],[],[]) 
+    for x in inputready:
+        if x == s:
+            conn,addr = s.accept()
+        else:
+            data = s.recv(buffsize)
+            if data:
+                print(data)
+            else:
+                x.close()
+                
+s.close()
