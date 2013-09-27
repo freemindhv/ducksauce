cb9f1b1acee39309975bed4d9d1efc737a8e87f4
freemind <freemind@freemind>
2013-09-27 21:35:22 +0200

    tested tcp

--- ideas/snippets/client.py
+++ ideas/snippets/client.py
@@ -2,11 +2,12 @@
 # Echo client program
 import socket
 
-HOST = 'localhost'    	  # The remote host
+HOST = ''                 # The remote host
 PORT = 50007              # The same port as used by the server
+data = 'Here comes a bunchload of data, 1101010101001101010101010101010101010101010101010010101010101010101010101010'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(bytes('Hello, world', "utf-8"))
-data = s.recv(1024)
+s.sendall(data)
 s.close()
-print("Received: {}".format(data.decode("utf-8")))
+print 'Data succesfully send'
+
