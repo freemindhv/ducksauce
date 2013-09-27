a5ba022498b43da5b0b04824349d1d449ee6991a
freemind <freemind@freemind>
2013-09-27 23:19:08 +0200

    added JSON MSG to client

--- ideas/snippets/client.py
+++ ideas/snippets/client.py
@@ -1,15 +1,18 @@
-#!/usr/bin/python 
-
+#!/usr/bin/env python3
 # Echo client program
 import socket
-
+import JSON
 host = "localhost"        # The remote host
 port = 50007              # The same port as used by the server
-data = b"Here comes a bunchload of data"
+data = JSON.Message01("Here comes a shitload of data")
+
+#convert to JSON
+x = data.toJSONString()
+y = JSON.Message02(1000, ["", "", ""]).toJSONString().encode()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-s.sendall(data)
+s.sendall(x.encode())
+s.sendall(y)
 s.close()
 print("Data succesfully send")
-
