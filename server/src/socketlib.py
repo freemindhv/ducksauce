ee516e9e895d82e414004b48b1a4670a51c72d2b
freemindhv <freemind@highvillage.de>
2013-09-28 03:06:30 +0200

    library

---
+++ server/src/socketlib.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import socket
+
+class DsSocket:
+    def __init__(self, host, port, backlog = 5):
+        self.h = host
+        self.p = port
+        self.b = backlog
+        
+    def socketInit(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    def socketBind(self):
+        try:
+            self.s.bind((self.h,self.p))
+        except socket.error as msg:
+            print("Could not open socket, maybe the port is already used?")
+            print("Error was {}" .format(msg))
+        self.s.listen(self.b)
