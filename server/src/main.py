61a3d6d2159478399358dfa1bd00614b243a6527
freemindhv <freemind@highvillage.de>
2013-09-28 03:05:48 +0200

    library

--- server/src/main.py
+++ server/src/main.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 import socket
 import select
+import socketlib
 
 host = socket.gethostbyname(socket.gethostname())
 port = 51001
 buffsize = 4096
+backlog = 5
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-try:
-    s.bind((host,port))
-except socket.error as msg:
-    print("Could not open socket, maybe the port is already used?")
-s.listen(5)
+a = socketlib.DsSocket(host, port, backlog)
+a.socketInit()
+a.socketBind()
 
 while True:
-    inputready,outputready,exceptready = select.select([s],[],[]) 
+    inputready,outputready,exceptready = select.select([a.s],[],[]) 
     for x in inputready:
-        if x == s:
-            conn,addr = s.accept()
+        if x == a.s:
+            conn,addr = a.s.accept()
         else:
-            data = s.recv(buffsize)
+            data = a.s.recv(buffsize)
             if data:
                 print(data)
             else:
                 x.close()
                 
-s.close()
+a.s.close()
