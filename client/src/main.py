507dc21ac37501a62c7bd661f6fbe458245b59dd
freemindhv <freemind@highvillage.de>
2013-09-30 21:50:09 +0200

    update client

--- client/src/main.py
+++ client/src/main.py
@@ -18,11 +18,19 @@
 ##!/usr/bin/env python3
 
 from modules.clientsocket import ClientSocket as ClientSocket
-from modules.serial import Encoder as Encoder
+from modules.objects import MessageHandler as MessageHandler
+from modules.serial import Decoder as Decoder
+from modules.messages import MessageId as MessageId
+from modules.messages import Ping as Ping
+from modules.messages import Acknowledgement as Acknowledgement
+from modules.messages import BillRequest as BillRequest
+from modules.messages import BillResponse as BillResponse
+from modules.messages import ReserveTableRequest as ReserveTableRequest
 
 client_socket = ClientSocket("127.0.0.1", 51001) #localhost for testing purposes
 
 #convert a testing message to JSON
-msg = ("Here comes a shitload of data")
+msg = Ping()
+
 
 client_socket.send(msg.serialize())
