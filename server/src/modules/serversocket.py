7149767042a9111873e2deb5b2c94521fe4c90c4
Steffen Nüssle <bockler@gmx.net>
2013-09-30 20:28:15 +0200

    changed libname

--- server/src/modules/serversocket.py
+++ server/src/modules/serversocket.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-import socket
 
-class DsSocket:
+import socket
+import select
+
+class ServerSocket:
     def __init__(self, host, port, backlog = 5):
-        self.h = host
-        self.p = port
-        self.b = backlog
-        
-    def socketInit(self):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
-    def socketBind(self):
+        
         try:
-            self.s.bind((self.h,self.p))
+            self.s.bind((host, port))
         except socket.error as msg:
-            print("Could not open socket, maybe the port is already used?")
-            print("Error was {}" .format(msg))
-        self.s.listen(self.b)
+            print("ERROR: bind() failed - {}" .format(msg))
+
+        self.s.listen(backlog)
+        
+    def accept(self):
+        return self.s.accept()
+    
+    def select(self, i = [], o = [], e = []):
+        return select.select(i, o, e)
+    
+    def fd(self):
+        return self.s
