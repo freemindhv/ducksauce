49c8d6781442c07233da93f93003c7073433f658
freemindhv <freemind@highvillage.de>
2013-09-30 21:35:02 +0200

    added libraries

---
+++ client/src/modules/clientsocket.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+#
+#    This file is part of ducksauce.
+#
+#    Ducksauce is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Ducksauce is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import socket
+
+class ClientSocket:
+    def __init__(self, server, port):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+        try:
+            self.s.connect((server, port))
+        except socket.error as msg:
+            print("ERROR: connect() failed - {}" .format(msg))
+            
+    def send(self,msg):
+        return self.s.sendall(msg)
